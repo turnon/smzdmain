953976fded0398d65021f94235f9e06d589551dc
ken <block24block@gmail.com>
2018-08-23 01:02:01 +0800

    colorize

--- smzdm.go
+++ smzdm.go
@@ -8,19 +8,21 @@ import (
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
+	"github.com/fatih/color"
 )
 
 type entry struct {
 	s *goquery.Selection
 }
 
-func (e *entry) toStr() string {
+func (e *entry) printf() {
 	title := strings.TrimSpace(e.s.Find(".feed-block-title a").First().Text())
 	price := strings.TrimSpace(e.s.Find(".feed-block-title a div").First().Text())
 	timeBlock := e.s.Find(".feed-block-extras").First()
 	timeBlock.Children().Remove()
 	time := strings.TrimSpace(timeBlock.Text())
-	return time + " " + title + " " + price
+	fmt.Printf("%-13s %s  ", time, title)
+	color.Green(price)
 }
 
 type search struct {
@@ -47,14 +49,13 @@ func (s *search) process() {
 	s.printKeyword()
 
 	doc.Find("#feed-main-list .z-feed-content").Each(func(i int, s *goquery.Selection) {
-		record := (&entry{s}).toStr()
-		fmt.Println(record)
+		(&entry{s}).printf()
 	})
 }
 
 func (s *search) printKeyword() {
 	dash := strings.Repeat("-", (20 - len(s.keyword)))
-	fmt.Println(s.keyword + " " + dash)
+	color.Red(s.keyword + " " + dash)
 }
 
 func main() {
