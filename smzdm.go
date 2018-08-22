f0df322dde9f45bc06e2facc2f722f1e74240690
ken <block24block@gmail.com>
2018-08-22 23:11:59 +0800

    extract time and encapsulate

--- smzdm.go
+++ smzdm.go
@@ -8,6 +8,19 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
+type entry struct {
+	s *goquery.Selection
+}
+
+func (e *entry) toStr() string {
+	title := strings.TrimSpace(e.s.Find(".feed-block-title a").First().Text())
+	price := strings.TrimSpace(e.s.Find(".feed-block-title a div").First().Text())
+	timeBlock := e.s.Find(".feed-block-extras").First()
+	timeBlock.Children().Remove()
+	time := strings.TrimSpace(timeBlock.Text())
+	return time + " " + title + " " + price
+}
+
 func main() {
 	resp, err := http.Get("http://search.smzdm.com/?c=home&s=%E5%BE%B7%E8%8A%99&v=b")
 
@@ -23,9 +36,8 @@ func main() {
 		panic(err)
 	}
 
-	doc.Find(".feed-block-title").Each(func(i int, s *goquery.Selection) {
-		title := strings.TrimSpace(s.Find("a").First().Text())
-		price := strings.TrimSpace(s.Find("a div").First().Text())
-		fmt.Println(title + " " + price)
+	doc.Find("#feed-main-list .z-feed-content").Each(func(i int, s *goquery.Selection) {
+		record := (&entry{s}).toStr()
+		fmt.Println(record)
 	})
 }
