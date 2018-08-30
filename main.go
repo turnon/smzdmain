5d3e882f6f838510a440ba163885b78def151433
ken <block24block@gmail.com>
2018-08-30 23:57:29 +0800

    handlers in seperate file

--- main.go
+++ main.go
@@ -2,11 +2,8 @@ package main
 
 import (
 	"flag"
-	"log"
 	"net/http"
-	"strings"
 	"sync"
-	"time"
 )
 
 const keysWorkerCount int = 2
@@ -68,50 +65,6 @@ func process(keywords []string, result output) output {
 	close(searchesCh)
 	resultWg.Wait()
 	return result
-}
-
-func static(keywords []string) func(writer http.ResponseWriter, request *http.Request) {
-	var (
-		timer      <-chan time.Time
-		m          sync.Mutex
-		htmlOutput output
-	)
-
-	fetch := func() {
-		m.Lock()
-		defer m.Unlock()
-
-		timer = time.After(60 * time.Second)
-		htmlOutput = new(html)
-		process(keywords, htmlOutput)
-		log.Println("pre-fetched")
-	}
-
-	fetch()
-
-	go func() {
-		for {
-			<-timer
-			fetch()
-		}
-	}()
-
-	return func(writer http.ResponseWriter, request *http.Request) {
-		m.Lock()
-		defer m.Unlock()
-		htmlOutput.print(writer)
-	}
-}
-
-func dyna(writer http.ResponseWriter, request *http.Request) {
-	q := request.URL.Query()["q"]
-	if len(q) == 0 {
-		return
-	}
-	keys := strings.Split(q[0], " ")
-	log.Println(request.RemoteAddr, keys)
-	htmlOutput := new(html)
-	process(keys, htmlOutput).print(writer)
 }
 
 func runServer(specificKeys ...[]string) {
